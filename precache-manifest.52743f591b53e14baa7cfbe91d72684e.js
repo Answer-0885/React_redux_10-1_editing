294df983f54afe6ec791e7debecf2c8e49de9221
Vitaly <answer-0885@mail.ru>
2023-03-02 14:06:14 +0700

    Updates

---
+++ precache-manifest.52743f591b53e14baa7cfbe91d72684e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5024f892bcb2171a406",
+    "url": "/React_redux_10-1_editing/static/css/main.5e8b564a.chunk.css"
+  },
+  {
+    "revision": "b5024f892bcb2171a406",
+    "url": "/React_redux_10-1_editing/static/js/main.b5024f89.chunk.js"
+  },
+  {
+    "revision": "514b93537dc4b9c69468",
+    "url": "/React_redux_10-1_editing/static/js/1.514b9353.chunk.js"
+  },
+  {
+    "revision": "a2dd341421a54659f07c",
+    "url": "/React_redux_10-1_editing/static/js/runtime~main.a2dd3414.js"
+  },
+  {
+    "revision": "319492370f0678e85ae7ea93314dcd5d",
+    "url": "/React_redux_10-1_editing/static/media/pencil.31949237.png"
+  },
+  {
+    "revision": "097aa470a2778ca3b3905c5b61674d0e",
+    "url": "/React_redux_10-1_editing/static/media/delete.097aa470.png"
+  },
+  {
+    "revision": "7264381d1e44467f7d5daeb56df091ab",
+    "url": "/React_redux_10-1_editing/index.html"
+  }
+];
